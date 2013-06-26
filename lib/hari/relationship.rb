b6961f8b4f27006f3a925ccbdef461db361821df
Victor Rodrigues <victorc.rodrigues@gmail.com>
2013-06-26 15:45:16 +0100

    Does not depend upon presence of node to create relationship id

--- lib/hari/relationship.rb
+++ lib/hari/relationship.rb
@@ -24,7 +24,7 @@ module Hari
     end
 
     def generate_id
-      "rel:#{start_node.id}:#{end_node.id}:#{backend.type}:#{SecureRandom.hex(3)}"
+      "rel:#{start_node_id}:#{end_node_id}:#{backend.type}:#{SecureRandom.hex(3)}"
     end
 
     def self.create(label, start_node, end_node, attrs = {})
