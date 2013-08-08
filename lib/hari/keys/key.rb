7e5b0799cf61f22b816b440da8c4c5f3af548c06
Victor Rodrigues <victorc.rodrigues@gmail.com>
2013-08-08 14:40:53 +0100

    Key common commands

--- lib/hari/keys/key.rb
+++ lib/hari/keys/key.rb
@@ -15,6 +15,34 @@ module Hari
         end
       end
 
+      def delete!
+        Hari.redis.del key
+      end
+
+      def exists?
+        Hari.redis.exists key
+      end
+
+      def expire(milliseconds)
+        Hari.redis.pexpire key, milliseconds
+      end
+
+      def expire_at(timestamp)
+        Hari.redis.expireat key, ::Time.parse(timestamp).to_s
+      end
+
+      def persist
+        Hari.redis.persist key
+      end
+
+      def type
+        Hari.redis.type key
+      end
+
+      def ttl
+        Hari.redis.ttl key
+      end
+
     end
   end
 end
---
+++ spec/hari/keys/key_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe Hari::Keys::Key do
+
+  let(:node) { Hari.node user: 10 }
+  subject    { node.list :friends }
+
+  specify { subject.instance_of? Hari::Keys::Key }
+
+  before { subject.add 1 }
+
+  specify '#exists? + #delete!' do
+    subject.exists?.should be_true
+
+    subject.delete!
+
+    subject.exists?.should be_false
+  end
+
+  specify '#type' do
+    subject.type.should eq('list')
+  end
+
+  specify '#expire + #persist + #ttl' do
+    subject.expire 100 # ms
+    (subject.ttl.to_i < 100).should be_true
+
+    sleep 0.1
+
+    subject.exists?.should be_false
+
+    subject.add 12
+    subject.expire 100
+    subject.persist
+
+    sleep 0.2
+
+    subject.exists?.should be_true
+  end
+
+end
