fc91e9c1733ae7380517841f508a9c195c3f7729
Victor Rodrigues <victorc.rodrigues@gmail.com>
2014-01-22 11:16:00 +0100

    Hari::Configuration docs

--- lib/hari/configuration.rb
+++ lib/hari/configuration.rb
@@ -1,9 +1,17 @@
 require 'hari/configuration/redis'
 
 module Hari
+  #
+  # Contains custom configuration modules for Hari
+  #
   module Configuration
     include Redis
 
+    # Configure Hari through this method
+    #
+    # @example Hari.configure { |c| c.redis = r }
+    # @return [void]
+    #
     def configure
       yield self if block_given?
     end
--- lib/hari/configuration/redis.rb
+++ lib/hari/configuration/redis.rb
@@ -1,34 +1,68 @@
 module Hari
   module Configuration
+    #
+    # Redis configuration module
+    #
     module Redis
 
+      # Returns redis client instance.
+      # If not defined one, uses Redis default
+      # configuration (localhost:6379)
+      #
+      # @return [Redis] redis client
+      #
       def redis
         @redis || begin
           self.redis = 'localhost:6379'
-          @redis
         end
       end
 
+      # Sets redis client
+      #
+      # @return [Redis] redis client
+      #
       def redis=(server)
         @redis = redis_namespace(server)
       end
 
+      # By default Hari will namespace all keys
+      # with the 'hari:' prefix. Calling this method,
+      # this namespacing is disabled
+      #
+      # @return [true]
+      #
       def disable_redis_namespace!
         @redis_namespace_disabled = true
       end
 
+      # @return [true] if redis namespace is disabled
+      # @return [false] if redis namespace is enabled
+      #
       def redis_namespace_disabled?
         @redis_namespace_disabled
       end
 
       private
 
+      # Namespaces the client with 'hari:' prefix,
+      # unless redis namespace is disabled
+      #
+      # @param [String, Hash, Redis] redis client or configuration
+      #
+      # @return [Redis] redis client
+      #
       def redis_namespace(server)
         return redis_server(server) if redis_namespace_disabled?
 
         ::Redis::Namespace.new 'hari', redis: redis_server(server)
       end
 
+      # Mounts a redis server instance when configuration is passed
+      #
+      # @param [String, Hash, Redis] redis client or configuration
+      #
+      # @return [Redis] redis client
+      #
       def redis_server(server)
         case server
         when String
